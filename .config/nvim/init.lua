4daf632dbd260fcfed0ff82478a33f981584b511
Julian West <jdub.writes.code@gmail.com>
2023-02-26 00:19:41 -0600

    cleanup

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,13 +1,19 @@
-	-- load seperate config files
+-- load seperate config files
+require("jwest.plugins-setup") -- lua/jwest/plugins-setup.lua
+require("jwest.core.options")
+require("jwest.core.keymaps")
+require("jwest.core.colorscheme")
 require('settings')    -- lua/settings.lua
 require('maps')        -- lua/maps.lua
 require('statusline')  -- lua/statusline.lua
-require('plugins')     -- lua/plugins.lua
-require('jwest.gitsigns') -- lua/jwest/gitsigns.lua
-require('jwest.lualine')
-require('jwest.lsp')
-require('jwest.telescope')
-require('jwest.treesitter')
-require('jwest.tokyonight')
-require('jwest.nvim-tree')
-require('jwest.bufferline')
+-- require('plugins')     -- lua/plugins.lua
+require("jwest.plugins.comment")
+-- require('jwest.gitsigns') -- lua/jwest/gitsigns.lua
+-- require('jwest.gitsigns') -- lua/jwest/gitsigns.lua
+-- require('jwest.lualine')
+-- require('jwest.lsp')
+-- require('jwest.telescope')
+-- require('jwest.treesitter')
+-- require('jwest.tokyonight')
+-- require('jwest.nvim-tree')
+-- require('jwest.bufferline')
